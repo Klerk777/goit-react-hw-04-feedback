56f19470e1b15072f6727a188fafd0d3432ea94c
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2023-01-24 16:00:08 +0200

    Adds some styles Changes to be committed: 	modified:   src/components/FeedbackOptions/FeedbackOptions.module.scss 	deleted:    src/components/Notification/Notification.module.scss 	modified:   src/components/Section/Section.module.scss 	new file:   src/components/Statistics/Statistic.module.scss 	modified:   src/components/Statistics/Statistics.jsx 	deleted:    src/components/Statistics/Statistics.modue.scss 	new file:   src/images/1695725.png

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Statistic.module.scss';
+import image from '../../images/1695725.png';
 
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {positivePercentage}%</li>
-    </ul>
+    <div className={styles.statisticWrap}>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive feedback: {positivePercentage}%</li>
+      </ul>
+      <img src={image} alt="cup of cofee" width="100px" />
+    </div>
   );
 }
 
